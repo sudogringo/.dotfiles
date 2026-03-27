51bbbc817f7cce4bbc53dd39a5069af03f79809e
Tiago Cunto <215190927+sudogringo@users.noreply.github.com>
2026-03-27 15:36:37 -0300

    fixed wall script
    swww to awww

--- Scripts/.scripts/wal-way.sh
+++ Scripts/.scripts/wal-way.sh
@@ -3,5 +3,5 @@
 # Wal config for wayland
 wal-way() {
     wal --backend colorz -n --cols16 -q -i "$@"
-    swww img "$(< "${HOME}/.cache/wal/wal")" --transition-type center
+    awww img "$(< "${HOME}/.cache/wal/wal")" --transition-type center
 }
