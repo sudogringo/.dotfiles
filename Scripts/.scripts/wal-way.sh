43b57b66553cbdbc45aa6423f75e1fe525145492
Tiago Cunto <215190927+sudogringo@users.noreply.github.com>
2026-04-17 09:13:23 -0300

    fixed wal

--- Scripts/.scripts/wal-way.sh
+++ Scripts/.scripts/wal-way.sh
@@ -2,6 +2,6 @@
 
 # Wal config for wayland
 wal-way() {
-    wal --backend colorz -n --cols16 -q -i "$@"
+    wal --backend colorz -n -q -i "$@"
     awww img "$(< "${HOME}/.cache/wal/wal")" --transition-type center
 }
